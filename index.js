2406dd0a58ceaf3965470ec0a2f88126acfe038f
kazimahin68 <kazimahin68@gmail.com>
2023-05-04 03:33:12 +0600

    Photo url included

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const chefs = require('./data/chefs.json');
 const port = process.env.PORT || 5000;
 
-
+app.use(cors())
 app.get('/', (req, res) =>{
     res.send("My Server for Mexican Cuisine")
 })
